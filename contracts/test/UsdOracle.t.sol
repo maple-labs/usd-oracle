cf73eea9b10f0e9bcc48f19de94be19884be2e98
Lucas Manuel <lucas@maple.finance>
2021-08-12 15:00:13 -0400

    chore: Update `main` branch for consumption (#4)
    * feat: add config file
    
    * refactor: Break out `usd-oracle` directory in maple-core to be a submodule dapptools project (SC-3068) (#1)
    
    * refactor: make contracts compile and introduce unit tests
    
    * chore: add github template
    
    * feat: update configs and ci file
    
    * fix: update config indentation
    
    * fix: update config indentation
    
    Co-authored-by: Lucas <lucas@maple.finance>
    
    * feat: Make Release (#2)
    
    * feat: Better Make Release (#3)
    
    - and take latest main oracle
    
    Co-authored-by: Lucas Manuel <lucasmanuel@MacBook-Pro-2.local>
    Co-authored-by: Satyam Agrawal <satyam0499@gmail.com>
    Co-authored-by: Michael De Luca <35537333+deluca-mike@users.noreply.github.com>

---
+++ contracts/test/UsdOracle.t.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+pragma solidity 0.6.11;
+
+import { DSTest } from "../../modules/ds-test/src/test.sol";
+import { UsdOracle } from "../UsdOracle.sol";
+
+contract UsdOracleTest is DSTest {
+
+    function test_usdOracle() public {
+        UsdOracle usd = new UsdOracle();
+
+        assertEq(usd.getLatestPrice(), 1 * 10 ** 8, "Incorrect oracle price");
+    }
+
+}
